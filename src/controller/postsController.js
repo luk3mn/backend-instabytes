57ee757368e9929cb65cd075cd274e9fc1570a58
luk3mn <lucasrenan2015@gmail.com>
2024-11-21 20:22:28 -0300

    feat: working on image upload by using POST

--- src/controller/postsController.js
+++ src/controller/postsController.js
@@ -1,6 +1,36 @@
-import { getTodosPosts } from "../models/postsModel.js";
+import { getTodosPosts, criarPost } from "../models/postsModel.js";
+import fs from "fs";
 
 export async function listarPosts(req, res) {
   const posts = await getTodosPosts();
   res.status(200).json(posts);
 } 
+
+export async function postarNovoPost(req, res) {
+  const novoPost = req.body;
+  try {
+    const postCriado = await criarPost(novoPost);
+    res.status(201).json(postCriado);
+  } catch (erro) {
+    console.error(erro.message);
+    res.status(500).json({"Erro":"Falha na requisição"});
+  }
+}
+
+export async function uploadImagem(req, res) {
+  const novoPost = {
+    descrcao: "",
+    imgUrl: req.file.originalname,
+    alt: ""
+  };
+
+  try {
+    const postCriado = await criarPost(novoPost);
+    const imagemAtualizada = `uploads/${postCriado.insertedId}.png`; // atribui o id do registro como o nome do arquivo;
+    fs.renameSync(req.file.path, imagemAtualizada);
+    res.status(201).json(postCriado);
+  } catch (erro) {
+    console.error(erro.message);
+    res.status(500).json({"Erro":"Falha na requisição"});
+  }
+}
--- src/routes/postsRoutes.js
+++ src/routes/postsRoutes.js
@@ -1,10 +1,28 @@
 import express from "express";
-import { listarPosts } from "../controller/postsController.js";
+import multer from "multer";
+import { listarPosts, postarNovoPost, uploadImagem } from "../controller/postsController.js";
+
+// Configura o armazenamento do Multer para uploads de imagens
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    // Especifica o diretório para armazenar as imagens enviadas
+    cb(null, 'uploads/'); // Substitua por seu caminho de upload desejado
+  },
+  filename: function (req, file, cb) {
+    // Mantém o nome original do arquivo por simplicidade
+    cb(null, file.originalname); // Considere usar uma estratégia de geração de nomes únicos para produção
+  }
+});
+
+const upload = multer({dest: "./uploads", storage});
+// linux ou mac pode ser usado apenas -> const upload = multer({dest:"./uploads"});
 
 const routes = (app) => {
   app.use(express.json()); // indica que será usado a estrutura JSON
 
-  app.get("/posts", listarPosts);
+  app.get("/posts", listarPosts); // ler
+  app.post("/posts",postarNovoPost); // cria
+  app.post("/upload", upload.single("imagem"), uploadImagem);
 }
 
 export default routes;
